d9f0e8ac0b1677f5efa25db398ef4660b36c7783
sparky12130 <parksm304@gmail.com>
2024-05-03 14:51:49 +0300

    finish the wordle game

--- js/index.js
+++ js/index.js
@@ -1,9 +1,94 @@
+const answer = "MONEY";
+
+let index = 0;
+let attempts = 0;
+let timer;
+
 function appStart() {
-  const handleKeydown = (event) => {
-    const key = event.key;
-    const keyCode = event.keyCode;
-    const thisBlock = document.querySelector(".board-block[data-index='00']");
-    thisBlock.innerText = key;
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "Game Over";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; position: absolute; top: 40vh; left: 30vw; background-color: white; width:200px; height:100px;";
+    document.body.appendChild(div);
   };
+
+  const Gameover = () => {
+    window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+
+  const nextLine = () => {
+    if (attempts === 6) return Gameover();
+    attempts++;
+    index = 0;
+  };
+
+  const handleEnterKey = () => {
+    let correct = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const letter = block.innerText;
+      const answer_letter = answer[i];
+      if (letter === answer_letter) {
+        correct++;
+        block.style.background = "#6AAA64";
+      } else if (answer.includes(letter)) block.style.background = "#C9B458";
+      else block.style.background = "#787C7E";
+      block.style.color = "white";
+    }
+
+    if (correct === 5) Gameover();
+    else nextLine();
+  };
+
+  const handleBackspace = (thisBlock) => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
+
+  const handleKeydown = (event) => {
+    const key = event.key.toUpperCase();
+    const keyCode = event.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index++;
+    }
+  };
+
+  const startTimer = () => {
+    const startTime = new Date();
+
+    function setTime() {
+      const nowTime = new Date();
+      const timePast = new Date(nowTime - startTime);
+      const min = timePast.getMinutes().toString().padStart(2, "0");
+      const sec = timePast.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${min}:${sec}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
   window.addEventListener("keydown", handleKeydown);
 }
+
+appStart();
